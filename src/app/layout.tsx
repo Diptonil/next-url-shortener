0afdaa8b38f6b41dd7a3933fb6677bc7bf984714
Thoroughfare by the Brooks <diptonilr@gmail.com>
2024-06-06 02:08:12 +0530

    Finish UI of main screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Shorty",
@@ -16,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-gradient-to-r from-slate-950 to-black">{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import Image from "next/image";
+"use client";
+
 import { Navbar } from "@/components/navbar";
+import { useState } from "react";
 
 
 export default function Home() {
+	const [url, setUrl] = useState("");
+
+	function convert(e: React.MouseEvent) {
+		e.preventDefault();
+		console.log(url);
+	}
+
     return (
         <main>
 			<Navbar />
+			<div className="flex">
+				<div className="mb-6 justify-center items-center flex flex-col">
+					<label className="block mb-2 text-sm font-medium text-gray-300">Link</label>
+					<input type="text" value={url} onChange={(e) => setUrl(e.target.value)} className="bg-transparent border border-pink-600 text-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[75vh] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-400 dark:focus:border-pink-400"></input>
+					<button onClick={convert} className="rounded-md slate-950 mt-6 bg-pink-800 px-8 py-4">Convert</button>
+				</div>
+			</div>
 		</main>
     );
 }
